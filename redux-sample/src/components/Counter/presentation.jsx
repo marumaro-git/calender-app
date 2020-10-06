b6fcd3ead309f501aa7873d449e470b0fd5716cb
maromaro <itmaromaro@gmail.com>
2020-10-07 01:30:55 +0900

    redux-tutorial

--- redux-sample/src/components/Counter/presentation.jsx
+++ redux-sample/src/components/Counter/presentation.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-const Counter = () => <div>counter</div>;
+// ==========ここから編集する==========
+const Counter = ({ count, increment, decrement }) => (
+  <>
+    <div>{count}</div>
+    <button onClick={() => increment(3)}>+</button>
+    <button onClick={() => decrement(1)}>-</button>
+  </>
+);
+// ==========ここまで編集する==========
 
 export default Counter;
